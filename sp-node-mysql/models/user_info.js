c57126c725116f42bbaf647474753623ac70cf2f
maquiles <mattaquiles@gmail.com>
2017-02-21 20:56:41 -0500

    update to user_info.js - model...  YEAH BOIIIIIIIIIIIIIII
    only need get by id and put for user_info since i will set up the
    cascade to post and delete with the users table
    
    XOXO,
    Matt

--- sp-node-mysql/models/user_info.js
+++ sp-node-mysql/models/user_info.js
@@ -1,22 +1,8 @@
 var db = require('../dbconn');
 
 var user_info={
-  /*dont really want to get all
-  getAllUserInfo:function(callback){
-    return db.query("select * from users", callback);
-  },*/
   getUserInfo:function(id, callback){
     return db.query("select * from user_info where user_id=?", [id], callback);
-  },
-  addUserInfo:function(User, callback){
-    return db.query("insert into users values(?,?,?,?)",
-      [User.user_id,
-       User.password,
-       User.create_date,
-       User.is_active], callback);
-  },
-  deleteUserInfo:function(id, callback){
-    return db.query("delete from users where user_id=?", [id], callback);
   },
   updateUserInfo:function(id, User, callback){
     return db.query("update users set password=?, create_date=?, is_active=? where user_id=?",
@@ -26,4 +12,4 @@ var user_info={
   }
 };
 
-module.exports = users;
+module.exports = user_info;
