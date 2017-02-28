0f155db12c487d7acb11bcd67b5893037eec6006
maquiles <mattaquiles@gmail.com>
2017-02-28 17:09:57 -0500

    updating functions in user_info model
    created add and delete functions in user_info model
    
    tried adding addUserInfo function into .post route of users route but
    it did not work since the parameters were not the same
    
    added variable to require user_info routes in app.js

--- sp-node-mysql/app.js
+++ sp-node-mysql/app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var user_info = require('./routes/user_info');
 
 var app = express();
 
--- sp-node-mysql/models/user_info.js
+++ sp-node-mysql/models/user_info.js
@@ -1,6 +1,58 @@
 var db = require('../dbconn');
 
 var user_info={
+  addUserInfo:function(Info, callback){
+    return db.query("insert into users values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",[
+      Info.user_id,
+      Info.user_id,
+      Info.first_name,
+      Info.last_name,
+      Info.middle_name,
+      Info.dob,
+      Info.mom_first_name,
+      Info.mom_middle_name,
+      Info.mom_last_name,
+      Info.marital_status,
+      Info.sex,
+      Info.race,
+      Info.religion,
+      Info.user_infocol,
+      Info.ssn,
+      Info.address,
+      Info.city,
+      Info.county,
+      Info.state,
+      Info.country,
+      Info.zip_code,
+      Info.county_code,
+      Info.home_phone,
+      Info.cell_phone,
+      Info.work_phone,
+      Info.prim_care_phys_name,
+      Info.prim_care_phys_addr,
+      Info.prim_care_phys_phone,
+      Info.nok_first_name,
+      Info.nok_last_name,
+      Info.nok_relationship,
+      Info.nok_cell_phone,
+      Info.nok_home_phone,
+      Info.emergency_contact_name,
+      Info.emergency_contact_addr,
+      Info.emergency_contact_relationship,
+      Info.emergency_contact_phone,
+      Info.guarantor_name,
+      Info.guarantor_addr,
+      Info.uarantor_phone,
+      Info.guarantor_relationship,
+      Info.guarantor_ssn,
+      Info.guarantor_dob,
+      Info.employer_company,
+      Info.employer_addr,
+      Info.employer_phone,
+      Info.employee_title,
+      Info.employer_hr_contact_name,
+      Info.employer_hr_contact_phone], callback);
+  },
   getUserInfo:function(id, callback){
     return db.query("select * from user_info where user_id=?", [id], callback);
   },
@@ -9,6 +61,9 @@ var user_info={
                     [User.password,
                      User.create_date,
                      User.is_active, id], callback);
+  },
+  deleteUserInfo:function(id, callback){
+    return db.query("delete from user_info where user_id=/", [id], callback);
   }
 };
 
