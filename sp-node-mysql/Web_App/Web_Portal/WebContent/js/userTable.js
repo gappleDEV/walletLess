e8ceb789412437128fb2ccf0efb413d3b8864a28
maquiles <mattaquiles@gmail.com>
2017-03-24 10:29:21 -0400

    ipchange for demo
    change of ipaddress in web app

--- sp-node-mysql/Web_App/Web_Portal/WebContent/js/userTable.js
+++ sp-node-mysql/Web_App/Web_Portal/WebContent/js/userTable.js
@@ -1,7 +1,7 @@
 var usersApp = angular.module('usersApp', ['ngRoute']);
 console.log("Loading table...");
 usersApp.controller('usersListCrtl', function($scope, $http) {
-	$http.get('http://155.246.138.85:3000/user_info').success(function(data) {
+	$http.get('http://172.17.10.14:3000/user_info').success(function(data) {
 		console.log(data);
 		$scope.userData = data;
 	});
