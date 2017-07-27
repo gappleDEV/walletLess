6cb86edeea4725079da821bfdcc7407d60f172d8
Matthew Aquiles <mattaquiles@gmail.com>
2017-07-26 22:08:25 -0400

    small amount of spring-boot fun

---
+++ spring-boot/src/main/java/testProject/Application.java
@@ -0,0 +1,11 @@
+package testProject;
+
+import org.springframework.boot.StringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+    public static void main(String[] args){
+        StringApplication.run(Application.class, agrs);
+    }
+}
