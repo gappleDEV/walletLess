94e15226bdbc1b23c09356d13398210125609404
Matthew Aquiles <mattaquiles@gmail.com>
2018-04-11 18:08:48 -0400

    working on getting connected to the aws db now

--- web-service/WalletLessWebService/src/main/java/com/walletless/walletlesswebservice/UserAccount.java
+++ web-service/WalletLessWebService/src/main/java/com/walletless/walletlesswebservice/UserAccount.java
@@ -1,18 +1,36 @@
 package com.walletless.walletlesswebservice;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
+@Entity
+@Table(name = "Users")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt",}, allowGetters = true)
 public class UserAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private final String id;
+
+    @NotBlank
     private final String username;
+
+    @NotBlank
     private final String password;
+
+    @NotBlank
     private final int acctType;
+
+    @NotBlank
     private final LocalDateTime createDate;
+
+    @NotBlank
     private final int isActive;
 
     public UserAccount(String id, String username, String password, int acctType, int isActive){
