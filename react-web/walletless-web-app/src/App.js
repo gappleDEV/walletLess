2bbacf08c9ef394adbb69135ab51572c89ea10bb
Matthew Aquiles <mattaquiles@gmail.com>
2018-06-05 18:12:24 -0400

    small changes to web app, hopefully will finish up tomorrow and deploy to aws for meeting

--- react-web/walletless-web-app/src/App.js
+++ react-web/walletless-web-app/src/App.js
@@ -8,10 +8,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to WalletLess</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Walletless is a consumer-friendly app that enables users and healthcare providers
+          to access stored information in a secure, convenient, accurate and timely manner.
+          Users have immediate and easy access to a cyber wallet containing documents,
+          insurance cards, and information that would normally be placed in wallets.
+          Ultimately, additional information can be stored limited only by the imagination
+          of the consumer. Healthcare providers with consent from their patients can
+          access authorized, accurate information and documents from the patient and,
+          thereby, reduce one of its biggest problems of incomplete and inaccurate
+          information. Walletless will also facilitate streamlined communication between
+          healthcare providers, patients and insurance companies. The app will provide an
+          unparalled retention and marketing tool for healthcare providers. Walletless is
+          the opportunity to get an edge in the new consumer driven healthcare market.
         </p>
       </div>
     );
