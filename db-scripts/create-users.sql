ae0c8fa00118cacdb4049dd3eee717ff84aaa69f
Matthew Aquiles <mattaquiles@gmail.com>
2018-06-05 17:48:32 -0400

    generated react web app

--- db-scripts/create-users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE 'Users' (
-  `id` float() NOT NULL,
-  `username` varchar(120) NOT NULL,
-  `password` varchar(120) NOT NULL,
-  `acctType` int(11) NOT NULL DEFAULT '0',
-  `createDate` datetime NOT NULL,
-  `isActive` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
