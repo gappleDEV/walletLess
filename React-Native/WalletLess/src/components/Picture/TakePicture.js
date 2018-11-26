c312874f734666f129a4169b03826597b63f3ebd
Dylan Johnson <gsoccer13@gmail.com>
2018-11-25 21:45:41 -0500

    picture component
    take picture component

---
+++ React-Native/WalletLess/src/components/Picture/TakePicture.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableHighlight,
+  Image
+} from 'react-native';
+import { RNCamera as Camera } from 'react-native-camera';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 5,
+    borderColor: '#FFF',
+    marginBottom: 15,
+  },
+  cancel: {
+    position: 'absolute',
+    right: 40,
+    bottom: 20,
+    backgroundColor: 'blue',
+    color: '#FFF',
+    fontWeight: '600',
+    fontSize: 17,
+  }
+});
+
+export default class TakePicture extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: null,
+    };
+  }
+
+  takePicture = async () => {
+    try {
+      const data = await this.camera.takePictureAsync();
+      this.setState({ path: data.uri });
+      // this.props.updateImage(data.uri);
+      // console.log('Path to image: ' + data.uri);
+    } catch (err) {
+      console.log('err: ', err);
+    }
+  };
+
+  renderCamera() {
+    return (
+      <Camera
+        ref={(cam) => {
+          this.camera = cam;
+        }}
+        style={styles.preview}
+        flashMode={Camera.Constants.FlashMode.off}
+        permissionDialogTitle={'Permission to use camera'}
+        permissionDialogMessage={'We need your permission to use your camera phone'}
+      >
+        <TouchableHighlight
+          style={styles.capture}
+          onPress={this.takePicture.bind(this)}
+          underlayColor="rgba(255, 255, 255, 0.5)"
+        >
+          <View />
+        </TouchableHighlight>
+      </Camera>
+    );
+  }
+
+  renderImage() {
+    return (
+      <View>
+        <Image
+          source={{ uri: this.state.path }}
+          style={styles.preview}
+        />
+        <Text
+          style={styles.cancel}
+          onPress={() => this.setState({ path: null })}
+        >Cancel
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.path ? this.renderImage() : this.renderCamera()}
+      </View>
+    );
+  }
+}
